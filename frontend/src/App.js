9b11ce6406f8cba9368f3f32d92e6f0061cb913b
Renato <renatomarquesteles@gmail.com>
2020-03-25 10:28:19 -0300

    frontend: conecta api e permite registrar nova ong

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 
 import './global.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import Routes from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <ToastContainer position="top-center" autoClose={3000} />
+    </>
+  );
 }
 
 export default App;
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const response = await api.post('ongs', data);
+
+      toast.success(
+        <div>
+          Cadastrado realizado com sucesso! <br /> Seu ID de acesso:
+          {response.data.id}
+        </div>,
+        {
+          autoClose: false,
+          draggable: false,
+          closeOnClick: false,
+        }
+      );
+      history.push('/');
+    } catch (err) {
+      toast.error('Ocorreu um erro no cadastro, tente novamente mais tarde.');
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -25,14 +66,41 @@ export default function Register() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Nome da ONG" />
-          <input type="email" placeholder="E-mail" />
-          <input placeholder="Whatsapp" />
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <input
+            placeholder="Whatsapp"
+            value={whatsapp}
+            onChange={e => setWhatsapp(e.target.value)}
+            required
+          />
 
           <div className="input-group">
-            <input placeholder="Cidade" />
-            <input placeholder="UF" style={{ width: '80px' }} />
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={e => setCity(e.target.value)}
+              required
+            />
+            <input
+              placeholder="UF"
+              style={{ width: '80px' }}
+              value={uf}
+              onChange={e => setUf(e.target.value)}
+              required
+            />
           </div>
 
           <button className="button" type="submit">
