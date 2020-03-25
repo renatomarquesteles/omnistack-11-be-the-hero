c906afaced8bfb7113ba2b0b635ed18e0e641f4e
Renato <renatomarquesteles@gmail.com>
2020-03-25 08:53:42 -0300

    frontend: adiciona e estiliza pagina de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './global.css';
+
+import Logon from './pages/Logon';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return <Logon />;
 }
 
 export default App;
