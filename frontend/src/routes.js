254150c7af23ba90dfc366505ea79586208b598e
Renato <renatomarquesteles@gmail.com>
2020-03-25 09:35:34 -0300

    frontend: adiciona e estiliza pagina de casos cadastrados

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Incidents from './pages/Incidents';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+        <Route path="/incidents" component={Incidents} />
       </Switch>
     </BrowserRouter>
   );
