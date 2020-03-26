dce2ef30f05096701813fc385273a9affafad6fb
Renato <renatomarquesteles@gmail.com>
2020-03-26 09:55:11 -0300

    mobile: feat: navegaçao e pagina de casos

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello Omnistack</Text>
-    </View>
-  );
+  return <Routes />;
 }
